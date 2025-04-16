d0d0b5190991ac2734bbcd9a7750278cefcfd0ac
azadoes2021 <azafree@daum.net>
2025-04-16 16:21:06 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Menu Icon
+let menu = document.querySelector('.menu-icon');
+
+menu.onclick = () => {
+  menu.classList.toggle("move");
+};
+// Category Swiper
+var swiper = new Swiper(".categorySwiper", {
+
+
+
+  // autoplay: {
+  //   delay: 4500,
+  //   disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Responsive for all devices
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    510: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    750: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
+
+
+
+
+
